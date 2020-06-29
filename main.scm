d2e5bb8322da624c9eec86659c037a3c65ef4068
LeeByeongdo <bdbdbdlee@gmail.com>
2020-06-29 14:41:29 +0000

    optimized

--- main.scm
+++ main.scm
@@ -4,8 +4,9 @@
 (define (f a b c) (
   cond 
     ((and (or (< a b) (= a b)) (or (< a c) (= a c))) (+ (square b) (square c)))
-    ((and (or (< b a) (= b a)) (or (< b c) (= b c))) (+ (square a) (square c)))
+    ((> c b) (+ (square a) (square c)))
     (else (+ (square a) (square b)))
-    ))
+  )
+)
 
-(f 5 2 1)
+(f 5 20 10)
